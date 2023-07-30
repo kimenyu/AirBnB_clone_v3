e5a2600163b29204590449adec4d6725ae6e2e71
Joseph Njoroge <91351673+kimenyu@users.noreply.github.com>
2023-07-30 18:52:32 +0300

    Update amenities.py

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-This file contains the Amenity module
+Create a new view for amenity objects that handles all default RESTFul API actions:
 """
 from api.v1.views import app_views
 from flask import jsonify, abort, request, make_response

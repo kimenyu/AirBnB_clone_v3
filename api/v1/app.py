6fe1de3960dbde8d029304721ea32222ba6a1c4a
kimenyu <njorogekimeneyu@gmail.com>
2023-07-30 15:08:06 +0000

    app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,16 +1,30 @@
 #!/usr/bin/python3
-from flask import Flask
+"""
+This module contains the principal application
+"""
 from models import storage
 from api.v1.views import app_views
-
+from flask import Flask, make_response, jsonify
+from os import getenv
+from flask_cors import CORS
+from flasgger import Swagger
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
 @app.teardown_appcontext
-def teardown_db(exception):
-    """Closes the storage on teardown"""
+def close_db(obj):
+    """ calls methods close() """
     storage.close()
-    
+
+
+
+
+
+
 if __name__ == "__main__":
-    app.run(host='HBNB_API_HOST', port='HBNB_API_PORT', threaded=True)
+
+    host = getenv('HBNB_API_HOST', default='0.0.0.0')
+    port = getenv('HBNB_API_PORT', default=5000)
+
+    app.run(host, int(port), threaded=True)
